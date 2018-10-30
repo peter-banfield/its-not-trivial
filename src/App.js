106e5f092294593bd9f6f320a973824b55858711
pbanf <peter.e.banfield@gmail.com>
2018-10-30 10:35:53 -0500

    Add StartGame component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Container } from 'reactstrap';
 import { Provider } from 'react-redux';
-import { CreateGame, RoomCode, PlaceBets, JoinRoom } from "./components"
+import { CreateGame, RoomCode, PlaceBets, JoinRoom, StartGame } from "./components"
 import Store from "./store"
 
 
@@ -17,6 +17,7 @@ class App extends Component{
                 <Route path="/RoomCode" component = {RoomCode} />
                 <Route path="/PlaceBets" component = {PlaceBets} />
                 <Route path="/JoinRoom" component = {JoinRoom} />
+                <Route path="/StartGame" component = {StartGame} />
             </Switch>
             </BrowserRouter>
             </Container>
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import CreateGame from './Gameboard/CreateGame'
 import RoomCode from './Gameboard/RoomCode'
 import PlaceBets from './Player/PlaceBets'
 import JoinRoom from './Player/JoinRoom'
+import StartGame from './Player/StartGame'
 
-export { CreateGame, RoomCode, PlaceBets, JoinRoom }
+export { CreateGame, RoomCode, PlaceBets, JoinRoom, StartGame }
