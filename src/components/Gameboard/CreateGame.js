c390c5135e2f2d80b53d85736cddb93899b02005
pbanf <peter.e.banfield@gmail.com>
2018-11-17 12:46:36 -0600

    Generate a unique room code and put it in the database
     - Add aws-sdk package to connect to dynamodb
     - Recursively attempt to put roomcode in table until a
    unique code is generated

--- src/components/Gameboard/CreateGame.js
+++ src/components/Gameboard/CreateGame.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 import io from 'socket.io-client';
+var AWS = require("aws-sdk");
 var randomstring = require("randomstring");
+AWS.config.region = 'us-east-1'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-east-1:80d57a52-edb2-44de-acb8-e6759c55991d',
+});
+var dynamodb = new AWS.DynamoDB();
+
 
 const socket = io('http://localhost:8080')
 
@@ -11,10 +18,19 @@ export default class CreateGame extends React.Component {
             // This binding is necessary to make `this` work in the callback
             this.gameCreate = this.gameCreate.bind(this);
         }
-
-        gameCreate = event => {
-            var roomcode = randomstring.generate({length: 4, charset: 'alphabetic'}).toUpperCase();
-            socket.emit("createroom", )
+        
+        gameCreate = event => {           
+            var generateuniquecode = function(){
+                var roomcode = randomstring.generate({length: 4, charset: 'alphabetic'}).toUpperCase();                
+                dynamodb.putItem({Item: {"id": {S: roomcode}, "CanJoin": {BOOL: true}},TableName: "Rooms",ConditionExpression:"attribute_not_exists(id)"}, function(err, res){
+                    if(err) generateuniquecode();
+                    else{
+                        // DO what is needed after a unique roomcode is generated and put in the database
+                    };
+                });
+            } 
+            generateuniquecode()
+            
         }
 
         render() {
