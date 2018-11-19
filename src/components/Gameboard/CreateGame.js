26b3a2b398d968ca253271f3eacb7b0bb2214705
pbanf <peter.e.banfield@gmail.com>
2018-11-19 15:57:41 -0600

    Move to the next screen
    Use state to move to the next screen.

--- src/components/Gameboard/CreateGame.js
+++ src/components/Gameboard/CreateGame.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Button } from 'reactstrap';
 import io from 'socket.io-client';
-var AWS = require("aws-sdk");
+import { Redirect } from 'react-router';
 var randomstring = require("randomstring");
+// the next 6 lines connect to the database
+var AWS = require("aws-sdk");
 AWS.config.region = 'us-east-1'; // Region
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: 'us-east-1:80d57a52-edb2-44de-acb8-e6759c55991d',
@@ -12,28 +15,42 @@ var dynamodb = new AWS.DynamoDB();
 
 const socket = io('http://localhost:8080')
 
-export default class CreateGame extends React.Component {
+export class CreateGame extends React.Component {
         constructor(props){
             super(props);
             // This binding is necessary to make `this` work in the callback
             this.gameCreate = this.gameCreate.bind(this);
+            this.toRoomcode = false;
+            // TODO make better variable name here
+            this.state = {
+                toDashboard: false,
+              }
         }
+
         
-        gameCreate = event => {           
+        gameCreate = event => {  
             var generateuniquecode = function(){
                 var roomcode = randomstring.generate({length: 4, charset: 'alphabetic'}).toUpperCase();                
                 dynamodb.putItem({Item: {"id": {S: roomcode}, "CanJoin": {BOOL: true}},TableName: "Rooms",ConditionExpression:"attribute_not_exists(id)"}, function(err, res){
-                    if(err) generateuniquecode();
+                    if(err){
+                        generateuniquecode();
+                    } 
                     else{
-                        // DO what is needed after a unique roomcode is generated and put in the database
+                        // Put roomcode in store here
                     };
                 });
             } 
             generateuniquecode()
-            
+            //move to next screen 
+            this.setState((state) => ({
+                toDashboard: true
+              }))
         }
-
+        
         render() {
+            if (this.state.toDashboard === true) {
+                return <Redirect to="/RoomCode" />
+            }
             return (
                 <div className="container-fluid d-flex align-items-center justify-content-center" style={{height: '100%'}}>
                     <Button size="lg" onClick={this.gameCreate}>Create Game</Button>
@@ -41,3 +58,4 @@ export default class CreateGame extends React.Component {
             )
         }
 }
+export default connect(null, null)(CreateGame)
