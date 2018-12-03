b2501125909c510e0f532ac929c0e021fe30e23d
Brendan Lane <ardongo.bl@gmail.com>
2018-12-02 23:31:23 -0600

    added bet sorting functionality to AnswerSeeBets.js

--- src/components/Gameboard/AnswerSeeBets.js
+++ src/components/Gameboard/AnswerSeeBets.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Question from './Question';
 
+var _ = require('lodash');
+
 class AnswerSeeBets extends React.Component {
 
     componentWillReceiveProps(nextProps){ 
@@ -73,9 +75,38 @@ class AnswerSeeBets extends React.Component {
     }
 }
 
+function sortBets(bets){
+    console.log(bets)
+
+
+    var betsArr = []
+    for(var user in bets){
+       for(var key in bets[user]){
+           var bet = bets[user][key]
+           if(key !== "doubleDown"){
+                betsArr.push(bet)
+           }
+       }
+    }
+    betsArr.sort(function(a, b){return b-a})
+    console.log(betsArr)
+    var countedBets = _.countBy(betsArr, Math.floor)
+    var countBetsArr = Object.keys(countedBets)
+    countBetsArr.sort(function(a,b){return b-a})
+
+    var newCountBets = []
+    countBetsArr.forEach((id) => {
+        newCountBets.push({answer: id, count: countedBets[id]})
+    })
+
+    console.log(newCountBets)
+    return betsArr
+
+}
+
 function mapStateToProps(state){
     return {
-        // variable to use in component: refrence to state
+        bets: sortBets(state.gameplay.questions[state.gameplay.room.questionNum].bets)
     }
 }
 
